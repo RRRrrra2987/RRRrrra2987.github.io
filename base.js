4e91b6de3b61631afb102172659e82efb68faada
RRRrrra2987 <77225385+RRRrrra2987@users.noreply.github.com>
2021-02-05 14:21:46 +0900

    Add files via upload

---
+++ base.js
@@ -0,0 +1,16 @@
+var i=1
+alert("(ﾟ∀ﾟ)ｱﾋｬ");
+alert("イェイ(ﾟ∀ﾟ)");
+const btn = document.querySelector('.btn-menu');
+const nav = document.querySelector('nav');
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('open-menu')
+  /* ↓ ここから追加 ↓ */
+    if (btn.innerHTML === 'メニュー') {
+    btn.innerHTML = '閉じる';
+    } else {
+    btn.innerHTML = 'メニュー';
+    }
+  /* ↑ ここまで追加 ↑ */
+});
